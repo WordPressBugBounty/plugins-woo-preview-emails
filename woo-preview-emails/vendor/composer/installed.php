3e9a97b7d95c4b07c072d85011a46e3188ab29c2
Sean Murphy <sean@iamseanmurphy.com>
2025-04-21 06:18:52 +0000

    2.2.14

--- woo-preview-emails/vendor/composer/installed.php
+++ woo-preview-emails/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codemanas/woo-preview-emails',
-        'pretty_version' => '2.2.13',
-        'version' => '2.2.13.0',
-        'reference' => 'c0320dc65341068de899e2486696a4476c3c7b46',
+        'pretty_version' => '2.2.14',
+        'version' => '2.2.14.0',
+        'reference' => 'b9c92633ff895209bd98dd53298ea492143ed969',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codemanas/woo-preview-emails' => array(
-            'pretty_version' => '2.2.13',
-            'version' => '2.2.13.0',
-            'reference' => 'c0320dc65341068de899e2486696a4476c3c7b46',
+            'pretty_version' => '2.2.14',
+            'version' => '2.2.14.0',
+            'reference' => 'b9c92633ff895209bd98dd53298ea492143ed969',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
